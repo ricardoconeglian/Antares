0fcce9a1dc9eb3467548ffdaeb81d59b4a55d93c
ricardoconeglian <72042399+ricardoconeglian@users.noreply.github.com>
2021-12-09 18:50:50 -0300

    inclusão de ServerErrorMessages component

---
+++ antares-frontend/src/app/shared/components/server-error-messages/server-error-messages.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServerErrorMessagesComponent } from './server-error-messages.component';
+
+describe('ServerErrorMessagesComponent', () => {
+  let component: ServerErrorMessagesComponent;
+  let fixture: ComponentFixture<ServerErrorMessagesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ServerErrorMessagesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServerErrorMessagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
