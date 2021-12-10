7b718a952fe79a1d03e9df865a3b70b1f6362951
ricardoconeglian <72042399+ricardoconeglian@users.noreply.github.com>
2021-12-10 07:43:10 -0300

    Iniciando contrução TipoManufaturaService

--- antares-frontend/src/app/shared/services/api-tipo-manufatura.service.ts
+++ antares-frontend/src/app/shared/services/api-tipo-manufatura.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiTipoManufaturaService {
 
-  constructor() { }
+
+  //monta a base da URL que será enviado a API
+  baseUrl = "http://127.0.0.1:8000/";
+  //Monta o cabeçalho do HTTP a ser enviado
+  httpHeaders = new HttpHeaders().set('Content-Type', 'application/json')
+  constructor(private http: HttpClient) { }
+
+  //Busca todos os produtos no banco de dados
+  getAllManuType(): Observable<any>{
+    return this.http.get(this.baseUrl + 'produto/cadastro-produto/',
+    {headers: this.httpHeaders})
+  }
+
+  //Busca o produto por ID, usado em Update e Delete
+  getProduct(id:any) : Observable<any>{
+    return this.http.get(this.baseUrl + 'produto/cadastro-produto/' + id + '/',
+    {headers: this.httpHeaders});
+
+  }
+
+  //Salva novo produto no banco de dados
+  saveNewProduct(produto:any) : Observable<any>{
+    return this.http.post(this.baseUrl + 'produto/cadastro-produto/', produto,
+    {headers: this.httpHeaders});
+
+  }
+
+  // Altera o produto
+  updateProduct(produto:any) : Observable<any>{
+    // body é o corpo com os dados alterados do JSON, precisa dele para funcionar
+    let body = {codigo_sap_produto: produto.codigo_sap_produto, nome_produto: produto.nome_produto, descricao_produto: produto.descricao_produto, utilizacao: produto.utilizacao}
+    return this.http.put(this.baseUrl + 'produto/cadastro-produto/' + produto.id + '/', body,
+    {headers: this.httpHeaders});
+  }
+
+  // deleta o produto
+  deleteProduct(produto:any) : Observable<any>{
+    return this.http.delete(this.baseUrl + 'produto/cadastro-produto/' + produto.id + '/',
+    {headers: this.httpHeaders});
+  }
+}
 }
