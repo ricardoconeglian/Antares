0a9d7ea94aafe9c3c3762eae8a0077568781bf97
ricardoconeglian <72042399+ricardoconeglian@users.noreply.github.com>
2021-12-15 14:30:28 -0300

    Implementado lista estoque insumo

--- antares-frontend/src/app/pages/components/cadastro/estoque-insumo/lista-estoque-insumo/lista-estoque-insumo.component.ts
+++ antares-frontend/src/app/pages/components/cadastro/estoque-insumo/lista-estoque-insumo/lista-estoque-insumo.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ApiEstoqueInsumoService } from 'src/app/shared/services/api-estoque-insumo.service';
 
 @Component({
   selector: 'app-lista-estoque-insumo',
@@ -7,9 +9,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaEstoqueInsumoComponent implements OnInit {
 
-  constructor() { }
+   //Cria um array com os dados de estoque insumo
+   estoqueInsumo = {id: '', descricao_insumo: '', unidade: '', quantidade: '', valor_unitario:'', valor_total: '', estoque_minimo: ''}
 
-  ngOnInit(): void {
-  }
+   //Array que recebera os estoque de insumo vindos do banco de dados
+   estoqueInsumos = [
+    {id: '', descricao_insumo: '', unidade: '', quantidade: '', valor_unitario:'', valor_total: '', estoque_minimo: ''}
+   ];
+
+
+   //recebe ID selecionado para delete
+   selected_id: number | any;
+
+   constructor(private router: Router,
+     private route: ActivatedRoute,
+     private api:ApiEstoqueInsumoService, //recebe as funções do Serviço APIEstoqueInsumoService
+     protected injector: Injector) {
+
+       this.carregarEstoqueInsumo() //carrega todos os estoque de insumo na tabela toda vez que inicializa a pagina
+       this.route = this.injector.get(ActivatedRoute); //Injeção de dependencia da rota
+
+      }
+
+   ngOnInit(): void {
+     //Pega o numero do id vindo da URL e passa como parametro no função carregaEstoqueInsumo()
+     this.route.paramMap.subscribe((param: ParamMap) => {
+       let id = parseInt(param.get('id') || '{}');
+       this.selected_id = id;
+
+     })
+   }
+
+   //Carrega os estoque de insumo que estão no banco de dados
+   carregarEstoqueInsumo = () => {
+     this.api.getAllEstoqueInsumo().subscribe(
+       data => {
+         this.estoqueInsumos = data
+         console.log(this.estoqueInsumos)
+         
+       },
+       error => {
+         console.log("Aconteceu um erro", error)
+       }
+     )
+   }
+
+    //Navega de volta para a tela de listagem de estoque insumo
+    navegarParaCadastraEstoqueInsumo(){
+     this.router.navigate(['insumo/estoque-insumo/new',])
+   }
+
+   /*-------------------------------------------------------
+     Deletar cadastro de insumo
+     -------------------------------------------------------
+   */
+     delete(estoqueInsumo:any) {
+       const mustDelete = confirm('Deseja realmente excluir?')
+       if(mustDelete){
+         this.api.deleteEstoqueInsumo(estoqueInsumo).subscribe(
+           data => {
+             let index:any;
+             this.estoqueInsumos.forEach((e, i) => {
+               if(e.id == estoqueInsumo.id)
+                index = i;
+             })
+
+             this.estoqueInsumos.splice(index, 1);
+           },
+           error => {
+             console.log("Aconteceu um erro", error);
+           }
+         )
+       }
+     }
+
+
 
 }
--- antares-frontend/src/app/shared/services/api-estoque-insumo.service.ts
+++ antares-frontend/src/app/shared/services/api-estoque-insumo.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiEstoqueInsumoService {
 
-  constructor() { }
+   //monta a base da URL que será enviado a API
+   baseUrl = "http://127.0.0.1:8000/";
+   //Monta o cabeçalho do HTTP a ser enviado
+   httpHeaders = new HttpHeaders().set('Content-Type', 'application/json')
+
+
+   constructor(private http: HttpClient) { }
+
+   //Busca todos os estoque de insumo no banco de dados
+   getAllEstoqueInsumo(): Observable<any>{
+     return this.http.get(this.baseUrl + 'insumo/estoque-insumo',
+     {headers: this.httpHeaders})
+   }
+
+   //Busca o estoque de insumo por ID, usado em Update e Delete
+   getEstoqueInsumo(id:any) : Observable<any>{
+     return this.http.get(this.baseUrl + 'insumo/estoque-insumo/' + id + '/',
+     {headers: this.httpHeaders});
+
+   }
+
+   //Salva novo estoque de insumo no banco de dados
+   saveEstoqueInsumo(estoqueInsumo:any) : Observable<any>{
+     return this.http.post(this.baseUrl + 'insumo/estoque-insumo/', estoqueInsumo,
+     {headers: this.httpHeaders});
+
+   }
+
+   // Altera o estoque de insumo
+   updateEstoqueInsumo(estoqueInsumo:any) : Observable<any>{
+     // body é o corpo com os dados alterados do JSON, precisa dele para funcionar
+     let body = {descricao_insumo: estoqueInsumo.descricao_insumo, unidade: estoqueInsumo.unidade, quantidade: estoqueInsumo.quantidade, valor_unitario: estoqueInsumo.valor_unitario, valor_total:estoqueInsumo.valor_total, estoque_minimo: estoqueInsumo}
+     return this.http.put(this.baseUrl + 'insumo/estoque-insumo/' + estoqueInsumo.id + '/', body,
+     {headers: this.httpHeaders});
+   }
+
+   // deleta o estoque de insumo
+   deleteEstoqueInsumo(estoqueInsumo:any) : Observable<any>{
+     return this.http.delete(this.baseUrl + 'insumo/cadastro-insumo/' + estoqueInsumo.id + '/',
+     {headers: this.httpHeaders});
+   }
+
+
+
+
 }
